32d584be24684f55b3c5d50bffd769b6fa06e604
Eren YILDIZ <emre.erenyildiz@gmail.com>
2023-10-03 22:23:30 +0300

    new file:   angle.js

---
+++ angle.js
@@ -0,0 +1,118 @@
+//add/change HTML elements
+
+const nameTag=document.createElement("h1");
+nameTag.innerHTML ="Eren";
+document.body.append(nameTag);
+
+const myList =document.querySelector("#fruit");
+const listItem =document.createElement("li");
+listItem.textContent ="mango";
+myList.insertBefore(listItem,myList.getElementsByTagName("li")[1]);
+
+//add/change CSS elements
+
+const title =document.getElementById("myTitle");
+
+title.style.backgroundColor = "blue";
+title.style.color = "white";
+title.style.fontFamily ="consolas";
+title.style.textAlign ="center";
+title.style.border ="2px solid";
+title.style.border.display="block";
+
+//events
+
+//const element =document.getElementById("myButton");
+//element.onclick =doSomething;
+
+//const element =document.getElementById("myText");
+//const element2 =document.getElementById("myDiv");
+//element.onload =doSomething;
+//element.onchange=doSomething;
+//element2.onmouseover = doSomething;
+function doSomething() {
+    alert("You did something!");
+    element2.style.backgroundColor ="red";
+}
+
+//.addEventListener(event,function,useCapture)
+//You can add many events handlers to one element
+//Even the same event that invokes different functions
+
+//const innerDiv =documnet.getElementById("innerDiv");
+
+//innerDiv.addEventListener("mouseover",changeBlue);
+
+function changeBlue(){
+    innerDiv.style.backgroundColor="blue";
+}
+
+//show/hide HTML elements
+
+const btn =document.querySelector("#btn");
+const myImg =document.querySelector("#myImg");
+
+btn.addEventListener("click",() =>{
+    console.log(myImg.style.display);
+    if(myImg.style.display == "none"){
+        myImg.style.display ="block";
+    }
+    else{
+        myImg.style.display ="none";
+    }
+})
+
+//detect key presses
+
+const myDiv= document.getElementById("div2");
+window.addEventListener("keydown",move);
+let x=0;
+let y=0;
+function move(event){
+    switch (event.key) {
+        case "ArrowDown":
+            y+=5;
+            div2.style.top=y+ "px";
+            break;
+        case "ArrowUp":
+            y-=5;
+            div2.style.top=y+ "px";
+            break;
+         case "ArrowRight":
+            x+=5;
+            div2.style.left= x + "px";
+            break;
+         case "ArrowLeft":
+            x-=5;
+            div2.style.left= x + "px";
+            break;
+        default:
+            break;
+    }
+}
+
+const anibtn =document.getElementById("anibtn");
+const myAnimation =document.getElementById("anidiv");
+
+anibtn.addEventListener("click",begin);
+
+function begin(){
+    let timerId =null;
+    let x2 = 0;
+    let y2 = 0;
+
+    timerId=setInterval(frame,5);
+
+    function frame() {
+        if(x2 >=200 || y2>=200 ){
+            clearInterval(timerId);
+        }
+        else{
+            y2+=1;
+            x2+=1;
+            myAnimation.style.left =x2 +"px";
+            myAnimation.style.top =y2 +"px";
+        }
+    }
+}
+
